3957f00a553bd3656df20d64bd490ad33a654359
Héctor Fornes <hector.fornes@nunsys.com>
2022-05-18 14:42:47 +0200

    Initial version of consultas generated by JHipster-6.10.5

---
+++ src/main/java/com/nunsys/consultas/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.nunsys.consultas.repository;
+
+import com.nunsys.consultas.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/nunsys/consultas/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.nunsys.consultas.service.dto;
